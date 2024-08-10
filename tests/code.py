e6321ef030d8cf57bcfa2c13236afd266cfd19ca
Juanse <jtaborda579@soyudemedellin.edu.co>
2024-08-10 11:06:13 -0500

    add the conditionals

--- tests/code.py
+++ tests/code.py
@@ -20,3 +20,13 @@ def div(num_1: float, num_2: float) -> float | str:
 num_1 = float(input("Ingrese el primer número: "))
 num_2 = float(input("Ingrese el segundo número: "))
 operacion = input("Seleccione la operación que desea realizar: ")
+
+if operacion == 'Suma':
+    print(suma(num_1, num_2))
+elif operacion == 'Resta':
+    print(resta(num_1, num_2))
+elif operacion == 'Multiplicación':
+    print(mult(num_1, num_2))
+elif operacion == 'División':
+    print(div(num_1, num_2))
+
