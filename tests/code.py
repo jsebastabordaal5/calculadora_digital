6fbb6ff10b2e96e6660ee67d78efa5b33d5f71ad
Juanse <jtaborda579@soyudemedellin.edu.co>
2024-08-10 10:59:58 -0500

    div function created

--- tests/code.py
+++ tests/code.py
@@ -8,4 +8,15 @@ def mult(num_1: float, num_2: float) -> float:
     return num_1 * num_2
 
 
+def div(num_1: float, num_2: float) -> float | str:
+    if num_2 != 0:
+        return num_1 / num_2
+    else:
+        return "Error!"
+
+
+
+
+
+
 
