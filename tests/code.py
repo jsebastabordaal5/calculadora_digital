3c1a8d319d375c3aa85405e85db83fb77bd7e25d
Juanse <jtaborda579@soyudemedellin.edu.co>
2024-08-10 11:02:32 -0500

    add the inputs

--- tests/code.py
+++ tests/code.py
@@ -1,8 +1,10 @@
 def suma(num_1: float, num_2: float) -> float:
     return num_1 + num_2
 
+
 def resta(num_1: float, num_2: float) -> float:
     return num_1 - num_2
+
 
 def mult(num_1: float, num_2: float) -> float:
     return num_1 * num_2
@@ -15,8 +17,6 @@ def div(num_1: float, num_2: float) -> float | str:
         return "Error!"
 
 
-
-
-
-
-
+num_1 = float(input("Ingrese el primer número: "))
+num_2 = float(input("Ingrese el segundo número: "))
+operacion = input("Seleccione la operación que desea realizar: ")
